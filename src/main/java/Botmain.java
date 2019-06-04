9c71e2897f4c049a463650b576063e2c1fd55666
Michael Papadopoulos <micpap25@gmail.com>
2019-06-04 14:07:13 -0400

    COMPLETE WORKING VERSION for build

--- src/main/java/Botmain.java
+++ src/main/java/Botmain.java
@@ -5,23 +5,38 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import javax.security.auth.login.LoginException;
 
 public class Botmain extends ListenerAdapter {
+
     public static void main(String[] args) throws LoginException {
         JDABuilder jdaB = new JDABuilder(AccountType.BOT);
         String token = "NTgzNjU3NTU5Nzk0MTIyNzUy.XPAerg.Nx7Nw1VJG5j3Ttc6Roi9RXpAT4Y";
         jdaB.setToken(token);
         jdaB.addEventListener(new Botmain());
-        jdaB.buildAsync();
+        jdaB.build();
     }
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
         if(event.getAuthor().isBot()){
             return;
         }
+
         System.out.println("Message recieved from " + event.getAuthor().getName() + ":" + event.getMessage().getContentDisplay());
+
         if(event.getMessage().getContentRaw().equals("bomb!help")){
             event.getChannel().sendMessage("``` Hi! I'm a minigame bot for playing tons of games!\n\n Minesweeper : bomb!mine\n Wires : bomb!wire @player1 @player2\n Server Scatter : bomb!scatter\n BOOM!```").queue();
         }
 
+        if (event.getMessage().getContentRaw().equals("bomb!mine")){
+
+        }
+
+        if (event.getMessage().getContentRaw().equals("bomb!wires")){
+
+        }
+
+        if (event.getMessage().getContentRaw().equals("bomb!sca")){
+
+        }
 
     }
 }
