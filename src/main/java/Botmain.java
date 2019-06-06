c0fd72eba0adb1b4f8711f6bd9b7d65351cb9f0f
Michael Papadopoulos <micpap25@gmail.com>
2019-06-06 12:14:44 -0400

    COMPLETE WORKING VERSION for build

--- src/main/java/Botmain.java
+++ src/main/java/Botmain.java
@@ -1,8 +1,11 @@
-import net.dv8tion.jda.core.*;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.util.concurrent.TimeUnit;
 
 public class Botmain extends ListenerAdapter {
 
@@ -22,21 +25,37 @@ public class Botmain extends ListenerAdapter {
 
         System.out.println("Message recieved from " + event.getAuthor().getName() + ":" + event.getMessage().getContentDisplay());
 
-        if(event.getMessage().getContentRaw().equals("bomb!help")){
+        Message message = event.getMessage();
+        String[] messageComponents = message.getContentRaw().split(" ");
+
+        if(messageComponents[0].equals("bomb!help")){
             event.getChannel().sendMessage("``` Hi! I'm a minigame bot for playing tons of games!\n\n Minesweeper : bomb!mine\n Wires : bomb!wire @player1 @player2\n Server Scatter : bomb!scatter\n BOOM!```").queue();
+            if(messageComponents.length > 10){
+                event.getChannel().sendMessage("```...dude, chill out a little...```").queueAfter(1, TimeUnit.SECONDS);
+            }
         }
 
-        if (event.getMessage().getContentRaw().equals("bomb!mine")){
-
-        }
-
-        if (event.getMessage().getContentRaw().equals("bomb!wires")){
-
-        }
-
-        if (event.getMessage().getContentRaw().equals("bomb!sca")){
+        if (messageComponents[0].equals("bomb!mine")){
 
         }
 
+        if (messageComponents[0].equals("bomb!wires")){
+            if(message.getMentionedUsers().size() != 2){
+                event.getChannel().sendMessage("```Please use the proper format! Type bomb!help for more info.```").queue();
+                return;
+            }
+            event.getChannel().sendMessage("```That's the right format! It's too bad I don't know how to play that!```").queue();
+
+        }
+
+        if (messageComponents[0].equals("bomb!scatter")){
+
+        }
+
+    }
+    public void sendPrivateMessage(User user, String content)
+    {
+        // notice that we are not placing a semicolon (;) in the callback this time!
+        user.openPrivateChannel().queue( (channel) -> channel.sendMessage(content).queue() );
     }
 }
